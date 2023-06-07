be74b71cc4fd95ad9dfb2f83f89d0419f4d8423c
Himanshu Gupta <himanshu.gupta25@aexp.com>
2023-06-07 10:15:04 +0530

    Template changes

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = `${process.env.REACT_APP_API_URL}`;
+const USER_API_BASE_URL = `${process.env.REACT_APP_API_URL}/api/users`;
 
 class UserService {
 
